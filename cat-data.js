58822bf2c7667111e5def92ebd941003869f4362
Mariah <mariahschock@gmail.com>
2022-05-05 11:16:27 -0700

    functions

--- cat-data.js
+++ cat-data.js
@@ -2,8 +2,9 @@ export const cats = [
     {
         name:'Leo',
         age: 5,
+        likes: 'aggressive cuddling',
         image: './assets/Leo.png',
     },
-    { name: 'TK', age: 3, image: './assets/TK.png' },
-    { name: 'Phoebe', age: 1, image: './assets/Phoebe.png' },
+    { name: 'TK', age: 3, likes: 'carbs', image: './assets/TK.png' },
+    { name: 'Phoebe', age: 1, likes: 'to wake up and choose violence', image: './assets/Phoebe.png' },
 ];
---
+++ utils.js
@@ -0,0 +1,17 @@
+export function renderCat(cat) {
+    const div = document.createElement('div');
+    div.classList.add('cat');
+
+    const h1 = document.createElement('h1');
+    h1.textContent = cat.name;
+
+    const img = document.createElement('img');
+    img.src = cat.img;
+
+    const p = document.createElement('p');
+    p.textContent = `${cat.name} is ${cat.age} and likes ${cat.likes}`;
+
+    div.append(h1, img, p);
+    return div;
+}
+
