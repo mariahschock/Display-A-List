d45f36633332164e11876b81e38fe487f3f32423
Mariah <mariahschock@gmail.com>
2022-05-05 10:55:59 -0700

    created new .js folders

---
+++ cat-data.js
@@ -0,0 +1,9 @@
+export const cats = [
+    {
+        name:'Leo',
+        age: 5,
+        image: './assets/Leo.png',
+    },
+    { name: 'TK', age: 3, image: './assets/TK.png' },
+    { name: 'Phoebe', age: 1, image: './assets/Phoebe.png' },
+];
